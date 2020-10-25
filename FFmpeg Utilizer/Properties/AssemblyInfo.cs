fa7ac7633db239ce231db4cb8b46cdf9fe76ab36
N0tiC <n0tic@live.se>
2020-10-25 19:25:02 +0100

    Assembly Update.

--- FFmpeg Utilizer/Properties/AssemblyInfo.cs
+++ FFmpeg Utilizer/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FFmpeg Utilizer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A UI/GUI application which utilizes external ffmpeg/ffplay to execute commands depending on user input. It aims to be very lightweight, portable and user friendly. It will not be using any third party libraries.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ByteVault Studio")]
 [assembly: AssemblyProduct("FFmpeg Utilizer")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
