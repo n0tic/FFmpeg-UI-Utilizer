f39ceeba47e53a315ce76777723582556e15721e
N0tiC <n0tic@live.se>
2020-11-04 20:41:17 +0100

    Version update

--- FFmpeg Utilizer/Core/Core.cs
+++ FFmpeg Utilizer/Core/Core.cs
@@ -28,7 +28,7 @@ namespace FFmpeg_Utilizer
         public static BuildTypes buildType = BuildTypes.Alpha;
         public static int majorVersion = 0;
         public static int minorVersion = 1;
-        public static int buildVersion = 2;
+        public static int buildVersion = 3;
 
         public enum BuildTypes
         {
